40f492e81369b9cead00ec9e7d29185c846510c3
Semyz Serhii <80937588895@mail.ru>
2017-12-14 20:26:26 +0200

    no message

--- home.rb
+++ home.rb
@@ -1,6 +1,7 @@
-random=Array.new(4){ Array.new(4){rand(-10..10)} }
-
-p random
-
-
-p 'cool stuff'
+p random=Array.new(4){ Array.new(4){rand(-10..10)} }.flatten.select(&:negative?).sum
+class String
+ def like_yes?
+  self =~ /^yes/
+ end
+end
+p a= {'yesye22s' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => 'yes', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'y2esteryesar' => 2014}.keys.map(&:to_s).select(&:like_yes?).length
---
+++ home_2.rb
@@ -0,0 +1,24 @@
+p 365*24
+p 365*10*24*60
+p 365*22*24*60*60
+p 943000000/60/60/24/365
+
+var1 = 5
+var2 = '2'
+var3= var1 + var2.to_f
+
+p var3
+
+puts 'Яке ваше ім\'я?'
+name=gets.chomp
+puts 'Яке вас звати по-батькові?'
+father_name=gets.chomp
+puts 'Яке ваше прізвище?'
+second_name=gets.chomp
+
+puts "Вас звати #{name} #{father_name} #{second_name}"
+
+puts 'Your favorite number?'
+number=gets.chomp.to_i
+puts "your new favorite and better number #{number + 1} ;-)"
+
