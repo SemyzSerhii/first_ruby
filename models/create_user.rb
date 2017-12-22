e0c4af91d88751250176c053466f58c9a639deab
Serhii Ovcharenko <serhii.ovcharenko@masterofcode.com>
2017-12-22 22:26:23 +0200

    no message

--- models/create_user.rb
+++ models/create_user.rb
@@ -2,13 +2,34 @@ require_relative 'user'
 
 user = User.new('Serhii', 'Ovcharenko', 22) { |age| age < 20 }
 
-puts 'currency'
+puts 'Currency'
 user.currency = gets.chomp.to_i
 
-puts 'money'
+puts 'Money'
 user.money = gets.chomp.to_i
 
-user.options = { has_vas: false, has_villa: false }
+user.options = {
+  has_vas: false,
+  has_villa: false
+}.freeze
 
 p user.report
 p user.reach?
+p User.all
+# define_method(gets.chomp) do
+#   p 'test'
+# end
+
+# Variables
+# test - local variable
+# $test - global variable
+# TEST - const variable
+# @test - instance variable
+# @@test - class variable
+
+# Data types
+# :test - symbol
+# 'test' - string
+# 1 - ingeger
+# [] - array
+# {} - hash
--- models/user.rb
+++ models/user.rb
@@ -3,7 +3,7 @@
 class User
   attr_accessor :fname, :lname, :age, :is_old, :currency, :money, :options
 
-  EMAIL = 'mail@example.com'
+  EMAIL = 'mail@example.com'.freeze
 
   def initialize(fname, lname, age)
     self.age = age
@@ -11,6 +11,10 @@ class User
     self.lname = lname
 
     self.is_old = yield age
+  end
+
+  def self.all
+    ['user1', 'user2'].freeze
   end
 
   def full_name
@@ -23,8 +27,27 @@ class User
   end
 
   def reach?
-    has_stuff = options[:has_villa] || options[:has_mercedes] || options[:is_reach]
-    has_stuff || (money * currency > 1000)
+    has_stuff? || (money * currency > 1000)
+  end
+
+  private
+
+  def has_stuff?
+    options[:has_villa] || options[:has_mercedes] || options[:is_reach]
   end
 end
 
+class Admin < User
+end
+
+# metaprogramming example
+# def att_my_accessor *params
+#   params.each do |param|
+#     define_method param do
+#       instance_variable_set("@#{param}", nil)
+#     end
+#     define_method "#{param}=" do |str|
+#       instance_variable_set("@#{param}", str)
+#     end
+#   end
+# end
